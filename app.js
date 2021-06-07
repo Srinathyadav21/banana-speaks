1c5607aa1b49d210fede1876ccc276d85e19b80d
Srinathyadav21 <84555988+Srinathyadav21@users.noreply.github.com>
2021-06-07 16:12:04 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var btntranslate = document.querySelector("#btn-translate")
-var Txtinput = document.querySelector("#txt-input")
-
-console.log(Txtinput)
+var btntranslate = document.querySelector("#btn-translate");
+var Txtinput = document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+// outputDiv.innerText="srinath yadav"
 function clickEventHandler(){
-    console.log("clicked!") 
-    console.log("input",Txtinput.value)
+   outputDiv.innerText="dkncksjc " + Txtinput.value
+    
 
 }
 btntranslate.addEventListener("click",clickEventHandler)
