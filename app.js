938219b9710ecb8cf7123f72fbfdb48beeb828ea
Srinathyadav21 <84555988+Srinathyadav21@users.noreply.github.com>
2021-06-09 15:41:55 +0530

    done

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var btntranslate = document.querySelector("#btn-translate");
 var Txtinput = document.querySelector("#txt-input");
-var outputDiv=document.querySelector("#output");
-// outputDiv.innerText="srinath yadav"
-function clickEventHandler(){
-   outputDiv.innerText="dkncksjc " + Txtinput.value
-    
+var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL ="	https://api.funtranslations.com/translate/hodor.json"
+function getTranslation(text) {
+   return serverURL + "?" + "text=" + text
 }
-btntranslate.addEventListener("click",clickEventHandler)
+
+function clickEventHandler() {
+   var inputText = Txtinput.value
+   fetch(getTranslation(inputText))
+      .then(response => response.json)
+      .then(json => {
+         var translatedText=json.contents.translated;
+         outputDiv.innerText=translatedText;
+      })
+}
+btntranslate.addEventListener("click", clickEventHandler)
